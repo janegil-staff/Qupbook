1b1e93b041c2c2223f4f8119aa33d04ee5c3a0cf
Jan Egil Staff Øvretveit <janstovr@Jan-sin-MacBook-Air.local>
2025-10-13 14:43:59 +0200

    Ad Routes and Models in BBackend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
 function App() {
+
+  const get = async() => {
+    const res = fetch("http://localhost:8001");
+    console.log(res);
+  }
+
+  get();
   return <div>welcome to frontend</div>;
 }
 
